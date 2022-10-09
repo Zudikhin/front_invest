4cb567d7f2f7db24056a6cef28bd31e0b24d34f1
zudikhin <uzudihin@gmail.com>
2022-10-10 00:02:26 +0300

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -6,104 +6,29 @@ $(document).ready(function () {
         $(".nav_mobile_drop").toggleClass("active");
     });
 
-    $(".blog_content_item_content_read_btn").click(function() {
-        $(this).parent().parent().parent().parent().addClass("active");
-    });
+    // $('.idea_slider').slick({
+    //     dots: true,
+    //     infinite: true,
+    //     arrows: false,
+    //     speed: 500,
+    //     fade: true,
+    //     cssEase: 'linear'
+    // });
 
-    $(".scroll_home").click(function() {
-        $(".nav_mobile_top_btn").removeClass("active");
-        $(".nav_mobile_drop").removeClass("active");
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $(".header").offset().top
-        }, 2000);
-    });
-
-    $(".scroll_game").click(function() {
-        $(".nav_mobile_top_btn").removeClass("active");
-        $(".nav_mobile_drop").removeClass("active");
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $(".game").offset().top - 120
-        }, 2000);
-    });
-
-
-    $(".scroll_blog").click(function() {
-        $(".nav_mobile_top_btn").removeClass("active");
-        $(".nav_mobile_drop").removeClass("active");
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $(".blog").offset().top - 120
-        }, 2000);
-    });
-
-    $('.game_one_slider').on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
-        var i = (currentSlide ? currentSlide : 0) + 1;
-        $(".game_one_count span").text(i + ' / ' + slick.slideCount);
-    });
-
-    $('.game_one_slider').slick({
-        dots: false,
-        infinite: true,
-        arrows: false,
+    $('.idea_slider').slick({
+        dots: true,
         speed: 500,
-        fade: true,
-        cssEase: 'linear'
-    });
-
-
-    $('.game_two_slider').on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
-        var i = (currentSlide ? currentSlide : 0) + 1;
-        $(".game_two_count span").text(i + ' / ' + slick.slideCount);
-    });
-
-    $('.game_two_slider').slick({
-        dots: false,
         infinite: true,
-        arrows: false,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-    });
+        slidesToShow: 2,
+        responsive: [
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
 
-    $('.game_three_slider').on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
-        var i = (currentSlide ? currentSlide : 0) + 1;
-        $(".game_three_count span").text(i + ' / ' + slick.slideCount);
-    });
-
-    $('.game_three_slider').slick({
-        dots: false,
-        infinite: true,
-        arrows: false,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-    });
-
-    $('.game_four_slider').on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
-        var i = (currentSlide ? currentSlide : 0) + 1;
-        $(".game_four_count span").text(i + ' / ' + slick.slideCount);
-    });
-
-    $('.game_four_slider').slick({
-        dots: false,
-        infinite: true,
-        arrows: false,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-    });
-
-    $('.game_five_slider').on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
-        var i = (currentSlide ? currentSlide : 0) + 1;
-        $(".game_five_count span").text(i + ' / ' + slick.slideCount);
-    });
-
-    $('.game_five_slider').slick({
-        dots: false,
-        infinite: true,
-        arrows: false,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-    });
 
 });
